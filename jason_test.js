0c23ac6522309b0e2956ba3b80e0f6c0896de060
unsik <fdew@naver.com>
2016-07-23 04:17:30 +0900

    Update jason_test.js

--- jason_test.js
+++ jason_test.js
@@ -1,13 +1,19 @@
-app.use(express.json());
-app.post('/rest',function(request,response){
-    request.accepts('application/json');
- 
-    // input message handling
-    json = request.body;
-    console.log('name is :'+json.name);
-    console.log('address is :'+json.address);
- 
-    // output message
-    response.json({result:'success'});
- 
-});
+var http = require('http')
+   ,qs = require('querystring');
+http.createServer(function (req, res) {
+   if( req.url == '/' && req.method == 'POST'){
+       var postBody = '';
+       req.on('data', function (data) {
+           postBody += data;
+        });
+       req.on('end', function () {
+           var post = qs.parse(postBody);
+           //post데이터확인
+           console.log(post['postname']);
+       });
+       res.end('true');
+   }else{
+       res.writeHead(404, {'Content-Type': 'text/plain'});
+       res.end('404 ERROR');
+   }
+}).listen(8001);
