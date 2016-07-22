cd10065305ab29ca91bf9be12d19fd57c179bc1e
unsik <fdew@naver.com>
2016-07-23 02:10:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-var express  = require('express'),
-    app = express();
-/*
-    Your middlewares or setups usually around here
-*/
-//API Router
-var router = express.Router();// calling the outside routes 
-var index = require('./routes/index').router;
-app.use('/',index);
-
-//start Server
-var server = app.listen(52222,function(){
- 
-   console.log("Listening to port %s",server.address().port);
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+//load customers route
+var customers = require('./routes/customers'); 
+var app = express();
+var connection  = require('express-myconnection'); 
+var mysql = require('mysql');
+// all environments
+app.set('port', process.env.PORT || 4300);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+/*------------------------------------------
+    connection peer, register as middleware
+    type koneksi : single,pool and request 
+-------------------------------------------*/
+app.use(
+    
+    connection(mysql,{
+        
+        host: 'localhost',
+        user: 'root',
+        password : '',
+        port : 3306, //port mysql
+        database:'nodejs'
+    },'request')
+);//route index, hello world
+app.get('/', routes.index);//route customer list
+app.get('/customers', customers.list);//route add customer, get n post
+app.get('/customers/add', customers.add);
+app.post('/customers/add', customers.save);//route delete customer
+app.get('/customers/delete/:id', customers.delete_customer);//edit customer route , get n post
+app.get('/customers/edit/:id', customers.edit); 
+app.post('/customers/edit/:id',customers.save_edit);
+app.use(app.router);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
